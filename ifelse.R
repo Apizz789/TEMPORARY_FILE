b214216be2fd8d77f096baeedacf9ec30262e214
62011019 <p.pluemapirak@gmail.com>
2022-03-24 22:43:56 +0700

    .

---
+++ ifelse.R
@@ -0,0 +1,12 @@
+x <- -5
+y <- 8
+if(x > 0){
+print("X is Non-negative number")
+} else {
+print("X is Negative number")
+}
+if(y > 0){
+print("Y is Non-negative number")
+} else {
+print("Y is Negative number")
+}
